2e785ccc5e8badecf1c3dc4147c32ff4d6f5f9a6
Koshelev Dmitrii <me@koshelev.net>
2023-07-05 16:44:06 +0300

    Добавить скрипт для расшифровки списков студентов

---
+++ decrypt.py
@@ -0,0 +1,50 @@
+import json
+import sys
+
+from cryptography.fernet import Fernet
+
+
+def env_extract() -> dict:
+    with open('env.json', 'r') as read_file:
+        env = json.load(read_file)
+        read_file.close()
+        return env
+
+
+def decrypt_file(file_path, key):
+    try:
+        cipher = Fernet(key)
+        with open(file_path, 'rb') as encrypted_file:
+            ciphertext = encrypted_file.read()
+    except FileNotFoundError:
+        print("Указанный файл не найден.")
+        sys.exit(1)
+    except IOError:
+        print("Ошибка при чтении файла.")
+        sys.exit(1)
+    else:
+        decrypted_content = cipher.decrypt(ciphertext)
+
+    decrypted_file_path = f'decrypted_{file_path}'
+
+    try:
+        with open(f'{decrypted_file_path}', 'wb') as decrypted_file:
+            decrypted_file.write(decrypted_content)
+    except IOError:
+        print("Ошибка при записи файла.")
+        sys.exit(1)
+    except:
+        print("Произошла неизвестная ошибка.")
+        sys.exit(1)
+    else:
+        print(f"Файл {file_path} успешно расшифрован и сохранён как {decrypted_file_path}.")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Необходимо указать путь к файлу в качестве аргумента командной строки.")
+        sys.exit(1)
+    env = env_extract()
+    key = env["key"]
+    file_path = sys.argv[1]
+    decrypt_file(file_path, key)
